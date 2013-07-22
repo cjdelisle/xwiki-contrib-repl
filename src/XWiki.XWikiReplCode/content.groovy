8185006f70331eb1e87866685b340b6fe994ce81
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-07-22 22:40:44 +0300

    groovy repl working

--- src/XWiki.XWikiReplCode/content.groovy
+++ src/XWiki.XWikiReplCode/content.groovy
@@ -1,3 +1,27 @@
-public String run(Object context, Object doc) {
-  return 'Hello World';
+import javax.script.ScriptEngineFactory;
+import org.xwiki.script.ScriptContextManager;
+import java.io.StringWriter;
+
+public String run(String content, Object services)
+{
+  def engineFactory = services.component.getInstance(ScriptEngineFactory.class, 'groovy');
+  def engine = engineFactory.getScriptEngine();
+  def sContextManager = services.component.getInstance(ScriptContextManager.class);
+  def sContext = sContextManager.getScriptContext();
+
+  def sw = new StringWriter();
+  def ow = sContext.getWriter();
+  def out = null;
+  try {
+    sContext.setWriter(sw);
+    out = engine.eval(content, sContext); 
+  } finally {
+    sContext.setWriter(ow);
+  }
+
+  if (out != null) {
+    sw.append('\n');
+    sw.append(out.toString());
+  }
+  return sw.toString();
 }
